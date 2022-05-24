64d271fcbd342efa2f52e5e17d7c4cf4ddfad8e2
Abhi088 <abhicoding8@gmail.com>
2022-05-25 03:43:04 +0530

    Did some styling

--- src/pages/DayManagement.tsx
+++ src/pages/DayManagement.tsx
@@ -69,32 +69,45 @@ const DayManagementPage: FC<Props> = (props) => {
     }
 
     return (
-        <div className=" flex flex-col h-screen justify-center items-center bg-gradient-to-r from-orange-300 to-pink-300">
-            <button type="button" className="border border-black bg-green-400 p-2 rounded-md" onClick={() => setCreateTaskFormDialogBox(true)}>Create Daily Task</button>
-            <CreateDailyTask className={`${createTaskFormDialogBox ? "absolute" : "hidden"}`} setCreateTaskFormDialogBox={setCreateTaskFormDialogBox}></CreateDailyTask>
-            <input type="date" onChange={e => setStartDate(e.target.value)} placeholder="Start Date" />
-            <input type="date" onChange={e => setEndDate(e.target.value)} placeholder="End Date" />
-            {dailyTasks && dailyTasks.map((dailyTask, dailyTaskKey) => {
-                return (
-                    <div key={dailyTaskKey} className="flex flex-col">
-                        <div className="flex flex-row items-center">
-                            <span className="text-xl font-bold">{dailyTask.title}</span>
-                            <span className="text-sm">{dailyTask.date.toString().substring(0, 10)}</span>
-                        </div>
-                        {dailyTask.tasks.map((task, taskKey) => {
-                            return (
-                                <div key={taskKey} className={`flex flex-row items-center justify-center ${task.completionStatus ? "bg-green-400" : "bg-red-400"}`}>
-                                    <input type="checkbox" defaultChecked={task.completionStatus} onChange={() => handleUpdateCompletionStatus(dailyTask.id, task.sNo, !task.completionStatus, dailyTaskKey, taskKey)} />
-                                    <span>{task.sNo}</span>
-                                    <span>{task.task}</span>
-                                    <textarea value={task.remarks} onChange={(e) => updateRemarks(e.target.value, dailyTaskKey, taskKey)} />
-                                    <button className="border border-black px-1" onClick={() => handleUpdateRemarks(dailyTask.id, task.sNo, dailyTaskKey, taskKey)}>Save Remarks</button>
-                                </div>
-                            );
-                        })}
+        <div className="p-2 flex flex-row space-x-2 min-h-screen max-h-max justify-center bg-gradient-to-r from-orange-300 to-pink-300">
+            <CreateDailyTask className={`${createTaskFormDialogBox ? "absolute self-center" : "hidden"}`} setCreateTaskFormDialogBox={setCreateTaskFormDialogBox}></CreateDailyTask>
+            <div className="border-2 p-2 rounded-sm flex flex-col space-y-3 w-1/4">
+                <button type="button" className="border border-black bg-green-400 p-2 rounded-md text-sm" onClick={() => setCreateTaskFormDialogBox(true)}>Create Daily Task</button>
+                <div className="flex flex-col space-y-1.5">
+                    <label className="font-bold text-lg">Filters:</label>
+                    <div className="flex flex-col space-y-0.5">
+                        <input className="outline-none p-0.5" type="date" onChange={e => setStartDate(e.target.value)} value={startDate} />
+                        <input className="outline-none p-0.5" type="date" onChange={e => setEndDate(e.target.value)} value={endDate} />
                     </div>
-                );
-            })}
+                </div>
+            </div>
+            <div className="w-3/4 border rounded-sm p-2 flex flex-col space-y-2">
+                {dailyTasks && dailyTasks.map((dailyTask, dailyTaskKey) => {
+                    return (
+                        <div key={dailyTaskKey} className="border rounded-sm px-1.5 pb-1.5 flex flex-col space-y-1">
+                            <div className="flex flex-row items-center justify-between">
+                                <span className="text-xl font-bold">{dailyTask.title}</span>
+                                <span className="text-sm">{dailyTask.date.toString().substring(0, 10)}</span>
+                            </div>
+                            {dailyTask.tasks.map((task, taskKey) => {
+                                return (
+                                    <div key={taskKey} className={`rounded-sm px-2 py-0.5 flex flex-row items-center justify-between ${task.completionStatus ? "bg-green-400" : "bg-red-400"}`}>
+                                        <div className="flex flex-row items-center space-x-1">
+                                            <input type="checkbox" defaultChecked={task.completionStatus} onChange={() => handleUpdateCompletionStatus(dailyTask.id, task.sNo, !task.completionStatus, dailyTaskKey, taskKey)} />
+                                            <span className="font-medium">{task.sNo}.</span>
+                                            <span>{task.task}</span>
+                                        </div>
+                                        <div className="flex flex-row items-center space-x-1">
+                                            <textarea className="text-sm h-7 rounded-sm p-0.5" value={task.remarks} onChange={(e) => updateRemarks(e.target.value, dailyTaskKey, taskKey)} />
+                                            <button className="border border-black px-1 rounded-sm text-sm text-gray-400 bg-yellow-900" onClick={() => handleUpdateRemarks(dailyTask.id, task.sNo, dailyTaskKey, taskKey)}>Save Remarks</button>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 }
--- src/utils/Dates.ts
+++ src/utils/Dates.ts
@@ -17,7 +17,5 @@ export function getTommorowDate(separator = '') {
     let month = newDate.getMonth() + 1;
     let year = newDate.getFullYear();
 
-    console.log(newDate)
-
     return `${year}${separator}${month < 10 ? `0${month}` : `${month}`}${separator}${date}`
 }
